115a66fd30ff87403d63b62f73562875861c55bf
ocelot8421 <8ocelot@gmail.com>
2022-05-24 18:33:23 +0200

    taking memo notes

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -7,14 +7,21 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.concurrent.atomic.AtomicLong;
 
 //Resource Controller
+//This code uses Spring @RestController annotation, which marks the class as a controller where every method returns a domain object instead of a view. It is shorthand for including both @Controller and @ResponseBody.
 @RestController
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    //can serve as a synonym @RequestMapping(method=GET)
+    //The @GetMapping annotation ensures that HTTP GET requests to /greeting are mapped to the greeting() method.
     @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "kutyusz", defaultValue = "World") String pompom) {
+    public Greeting greeting(
+            // @RequestParam binds the value of the query string parameter name into the name parameter of the greeting() method. If the name parameter is absent in the request, the defaultValue of World is used.
+            @RequestParam(value = "kutyusz", defaultValue = "World") String pompom
+    ) {
+        // The Greeting object must be converted to JSON. Thanks to Spring’s HTTP message converter support, you need not do this conversion manually. Because Jackson 2 is on the classpath, Spring’s MappingJackson2HttpMessageConverter is automatically chosen to convert the Greeting instance to JSON.
         return new Greeting(counter.incrementAndGet(), String.format(template, pompom));
     }
 }
--- src/main/java/com/example/restservice/RestServiceApplication.java
+++ src/main/java/com/example/restservice/RestServiceApplication.java
@@ -3,10 +3,12 @@ package com.example.restservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// @Configuration within SpringBootConfiguration, @EnableAutoConfiguration, @ComponentScan
 @SpringBootApplication
 public class RestServiceApplication {
 
 	public static void main(String[] args) {
+		// SpringApplication.run()  method to launch an application.
 		SpringApplication.run(RestServiceApplication.class, args);
 	}
 
