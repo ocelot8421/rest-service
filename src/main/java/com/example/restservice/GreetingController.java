4703bf74fdf6c011e76c990fa8e8aef843142ee9
ocelot8421 <8ocelot@gmail.com>
2022-05-24 17:39:11 +0200

    Initial commit

---
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -0,0 +1,20 @@
+package com.example.restservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+//Resource Controller
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "kutyusz", defaultValue = "World") String pompom) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, pompom));
+    }
+}
